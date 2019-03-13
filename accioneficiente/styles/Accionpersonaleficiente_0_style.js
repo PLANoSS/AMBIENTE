d9f3d814292b8d23cf22304a733ea135efdfc7e3
PLANoSS <freyesbueno@gmail.com>
2019-03-13 11:17:39 -0500

    Acción personal eficiente

--- accioneficiente/styles/Accionpersonaleficiente_0_style.js
+++ accioneficiente/styles/Accionpersonaleficiente_0_style.js
@@ -6,7 +6,7 @@ var style_Accionpersonaleficiente_0 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    var value = feature.get("q2wHide_Reciclar y");
+    var value = feature.get("Reciclar y reusar (%)");
     var labelText = "";
     size = 0;
     var labelFont = "9.75px \'MS Shell Dlg 2\', sans-serif";
